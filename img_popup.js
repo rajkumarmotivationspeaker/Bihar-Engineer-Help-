e9e51d59b921ede08794307c8058ea323f419563
rajkumarmotivationspeaker <162527273+rajkumarmotivationspeaker@users.noreply.github.com>
2025-09-12 02:19:19 +0530

    Add files via upload

---
+++ img_popup.js
@@ -0,0 +1,21 @@
+let a = document.getElementById("pop_cross")
+console.log(a)
+
+let b = document.getElementById("pop_img")
+console.log(b)
+
+a.style.display = "none"
+b.style.display = "none"
+
+function showpop(){
+    a.style.display = "block"
+    b.style.display = "block"
+}
+
+function hidepop(){
+    a.style.display = "none"
+    b.style.display = "none"
+    
+}
+
+setTimeout(showpop, 8000)
